5006dbad8e3f2575412c01b7eb6b947c8ca2aac8
nick <nflower@asteria.com>
2024-03-04 09:03:01 +0900

    Checksum dependency

--- build/build.rs
+++ build/build.rs
@@ -2,6 +2,7 @@ extern crate winresource;
 
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        /* Set windows specific steps like Icon */
         let mut res = winresource::WindowsResource::new();
         res.set_icon("./build/gman.ico");
         res.compile().unwrap();
