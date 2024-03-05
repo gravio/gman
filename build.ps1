4cb8d110a8366c2d6887cd60adcf59344670dbde
nick <nflower@asteria.com>
2024-03-05 18:39:08 +0900

    Improved windows build script

--- build.ps1
+++ build.ps1
@@ -14,7 +14,7 @@ checksums -v -c $target_dir --force
 
 # zip the file for packaing
 $compress = @{
-    Path             = "$target_dir\${bin_name}.exe", "$target_dir\sbom.spdx", "$target_dir\release.hash"
+    Path             = "$target_dir\$bin_name.exe", "$target_dir\sbom.spdx", "$target_dir\release.hash"
     CompressionLevel = "Optimal"
     DestinationPath  = "$target_dir\${bin_name}_win_x64.zip"
 }
