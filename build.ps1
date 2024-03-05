fbee409b88e47d5886dcc54ea2d8bacbdf23d6d1
nick <nflower@asteria.com>
2024-03-05 18:38:59 +0900

    Improved windows build script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 $mode = "--release"
 $target_dir = ".\target\release"
+$bin_name = "graviomanager"
 
 # build the binary 
 cargo build $mode
@@ -8,4 +9,16 @@ cargo build $mode
 cargo sbom > $target_dir\sbom.spdx
 
 # generate checksum of artifacts
+checksums -v -c $target_dir --force
+
+
+# zip the file for packaing
+$compress = @{
+    Path             = "$target_dir\${bin_name}.exe", "$target_dir\sbom.spdx", "$target_dir\release.hash"
+    CompressionLevel = "Optimal"
+    DestinationPath  = "$target_dir\${bin_name}_win_x64.zip"
+}
+Compress-Archive @compress -Force
+
+# zip file re-checksum
 checksums -v -c $target_dir --force
